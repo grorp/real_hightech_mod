406886b3838f15f8306c9e205791bd50d66c386c
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-04-20 12:38:31 +0200

    Move the Lua code to /src

---
+++ src/init.lua
@@ -0,0 +1,8 @@
+dofile(minetest.get_modpath("hightech") .. "/src/dark.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/glass.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/stripes.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/doors.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/sign.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/tech_card_api.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/tech_card.lua")
+dofile(minetest.get_modpath("hightech") .. "/src/handcuffs.lua")
