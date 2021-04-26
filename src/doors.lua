bf6dc033991f6dbc665375da440addc2b11c0788
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-04-26 19:58:05 +0200

    Add locked doors

--- src/doors.lua
+++ src/doors.lua
@@ -21,6 +21,29 @@ minetest.register_craft({
 })
 
 doors.register(
+	"hightech:door_locked",
+	{
+		description = "Hightech Door\n(locked)",
+		tiles = {"hightech_door.png"},
+		inventory_image = "hightech_door_inv.png",
+		use_texture_alpha = "blend",
+		paramtype = "light",
+		light_source = minetest.LIGHT_MAX,
+		protected = true,
+		groups = {cracky = 3},
+		sounds = default.node_sound_stone_defaults(),
+	}
+)
+minetest.register_craft({
+	type = "shapeless",
+	recipe = {
+		"hightech:door",
+		"basic_materials:padlock",
+	},
+	output = "hightech:door_locked",
+})
+
+doors.register(
 	"hightech:door_no_window",
 	{
 		description = "Hightech Door\n(no window)",
@@ -37,4 +60,24 @@ minetest.register_craft({
 		{"hightech:dark", "hightech:dark"},
 	},
 	output = "hightech:door_no_window",
+})
+
+doors.register(
+	"hightech:door_no_window_locked",
+	{
+		description = "Hightech Door\n(no window, locked)",
+		tiles = {"hightech_door_no_window.png"},
+		inventory_image = "hightech_door_no_window_inv.png",
+		protected = true,
+		groups = {cracky = 3},
+		sounds = default.node_sound_stone_defaults(),
+	}
+)
+minetest.register_craft({
+	type = "shapeless",
+	recipe = {
+		"hightech:door_no_window",
+		"basic_materials:padlock",
+	},
+	output = "hightech:door_no_window_locked",
 })
