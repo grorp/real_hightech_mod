e6e07d54030a2594b5e433ad1fc5fe4870b8b6d4
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-05-05 21:59:12 +0200

    Remove locked doors
    They were replaced by Hightech Grids.

--- src/doors.lua
+++ src/doors.lua
@@ -23,29 +23,6 @@ minetest.register_craft({
 })
 
 doors.register(
-	"hightech:door_locked",
-	{
-		description = S("Hightech Door\n(locked)"),
-		tiles = {"hightech_door.png"},
-		inventory_image = "hightech_door_inv.png",
-		use_texture_alpha = "blend",
-		paramtype = "light",
-		light_source = minetest.LIGHT_MAX,
-		protected = true,
-		groups = {cracky = 3},
-		sounds = default.node_sound_stone_defaults(),
-	}
-)
-minetest.register_craft({
-	type = "shapeless",
-	recipe = {
-		"hightech:door",
-		"basic_materials:padlock",
-	},
-	output = "hightech:door_locked",
-})
-
-doors.register(
 	"hightech:door_no_window",
 	{
 		description = S("Hightech Door\n(no window)"),
@@ -62,24 +39,4 @@ minetest.register_craft({
 		{"hightech:dark", "hightech:dark"},
 	},
 	output = "hightech:door_no_window",
-})
-
-doors.register(
-	"hightech:door_no_window_locked",
-	{
-		description = S("Hightech Door\n(no window, locked)"),
-		tiles = {"hightech_door_no_window.png"},
-		inventory_image = "hightech_door_no_window_inv.png",
-		protected = true,
-		groups = {cracky = 3},
-		sounds = default.node_sound_stone_defaults(),
-	}
-)
-minetest.register_craft({
-	type = "shapeless",
-	recipe = {
-		"hightech:door_no_window",
-		"basic_materials:padlock",
-	},
-	output = "hightech:door_no_window_locked",
 })
