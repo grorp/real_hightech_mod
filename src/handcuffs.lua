b39b6618ec84e641e65ac5f13e599e7ce4364079
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-05-05 22:24:19 +0200

    Open the handcuffs with the right instead of the left mouse button

--- src/handcuffs.lua
+++ src/handcuffs.lua
@@ -59,56 +59,62 @@ end
 
 minetest.register_privilege("handcuff", S("Can handcuff other players"))
 
+local handcuffs_on_use = function(itemstack, player, pointed_thing)
+	if pointed_thing.type == "object" and pointed_thing.ref:is_player() then
+		local victim = pointed_thing.ref
+		local victim_meta = victim:get_meta()
+		if victim_meta:get_string("handcuffed_by") == "" then
+			if not minetest.check_player_privs(player:get_player_name(), "handcuff") then
+				minetest.chat_send_player(
+					player:get_player_name(),
+					S('You are not allowed to use Hightech Handcuffs (missing "handcuff" privilege).')
+				)
+				return
+			end
+			handcuff(player, victim)
+			minetest.chat_send_player(
+				victim:get_player_name(),
+				S("@1 has handcuffed you.", player:get_player_name())
+			)
+		else
+			minetest.chat_send_player(
+				player:get_player_name(),
+				S("@1 is already handcuffed by @2.", victim:get_player_name(), victim_meta:get_string("handcuffed_by"))
+			)
+		end
+	end
+end
+
+local handcuffs_on_secondary_use = function(itemstack, player, pointed_thing)
+	local player_meta = player:get_meta()
+	local player_victims = minetest.deserialize(player_meta:get_string("handcuffed_players"))
+	if player_victims == nil then
+		player_victims = {}
+	end
+	for _, victim_name in pairs(player_victims) do
+		local victim = minetest.get_player_by_name(victim_name)
+		un_handcuff(player, victim)
+		minetest.chat_send_player(
+			victim:get_player_name(),
+			S("@1 has opened your handcuffs.", player:get_player_name())
+		)
+	end
+end
+
 minetest.register_craftitem(
 	"hightech:handcuffs",
 	{
-		description = S("Hightech Handcuffs"),
+		description = minetest.colorize("#00fffb", S("Hightech Handcuffs")) .. "\n" .. S("PUNCH somebody to handcuff them.\nUSE to open the handcuffs."),
 		inventory_image = "hightech_handcuffs.png",
 		stack_max = 1,
-		on_use = function(itemstack, player, pointed_thing)
-			if pointed_thing.type == "object" and pointed_thing.ref:is_player() then
-				local victim = pointed_thing.ref
-				local victim_meta = victim:get_meta()
-				if victim_meta:get_string("handcuffed_by") == "" then
-					if not minetest.check_player_privs(player:get_player_name(), "handcuff") then
-						minetest.chat_send_player(
-							player:get_player_name(),
-							S('You are not allowed to use Hightech Handcuffs (missing "handcuff" privilege).')
-						)
-						return
-					end
-					handcuff(player, victim)
-					minetest.chat_send_player(
-						victim:get_player_name(),
-						S("@1 has handcuffed you.", player:get_player_name())
-					)
-				else
-					minetest.chat_send_player(
-						player:get_player_name(),
-						S("@1 is already handcuffed by @2.", victim:get_player_name(), victim_meta:get_string("handcuffed_by"))
-					)
-				end
-			else
-				local player_meta = player:get_meta()
-				local player_victims = minetest.deserialize(player_meta:get_string("handcuffed_players"))
-				if player_victims == nil then
-					player_victims = {}
-				end
-				for _, victim_name in pairs(player_victims) do
-					local victim = minetest.get_player_by_name(victim_name)
-					un_handcuff(player, victim)
-					minetest.chat_send_player(
-						victim:get_player_name(),
-						S("@1 has opened your handcuffs.", player:get_player_name())
-					)
-				end
-			end
-		end
+		on_use = handcuffs_on_use,
+		on_place = handcuffs_on_secondary_use,
+		on_secondary_use = handcuffs_on_secondary_use,
 	}
 )
 
 minetest.register_on_leaveplayer(
-	function(player, timed_out)
+	function(player)
 		local player_meta = player:get_meta()
 
 		if player_meta:get_string("handcuffed_by") ~= "" then
