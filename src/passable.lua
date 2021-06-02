f20d6dea7481d2a592b059c6ef9c2e3d24721501
Gregor Parzefall <gregor.parzefall@posteo.de>
2021-06-02 16:23:54 +0200

    Add blocks where you can walk through

---
+++ src/passable.lua
@@ -0,0 +1,12 @@
+local S = minetest.get_translator("hightech")
+
+minetest.register_node(
+	"hightech:dark_passable",
+	{
+		description = S("Passable Dark Hightech"),
+		tiles = {"hightech_dark.png"},
+    walkable = false,
+		groups = {cracky = 3},
+		sounds = default.node_sound_stone_defaults(),
+	}
+)
